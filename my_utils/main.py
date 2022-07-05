0058befe3b3c823c435de8fa54fdccb6c8a9fc49
Cooper <cooperxmiller@gmail.com>
2022-07-04 20:18:14 -0500

    made open apps command more dynamic

--- my_utils/main.py
+++ my_utils/main.py
@@ -10,15 +10,23 @@ def main():
 
 
 @app.command()
-def start_up():
-    """Start up normal applications"""
-    typer.echo(f"Welcome! Starting up...")
+def start_up(apps: str = "default"):
+    """Start up applications
+    
+    Args:
+        apps (str): Type of applications to open based on my-apps.json key values.
+    """
+    typer.echo(f"Welcome! Starting up Applications...")
+    match apps:
+        case "default":
+            typer.echo(f"Opening System Applications...")
+            utils.open_apps("installed")
 
-    typer.echo(f"Opening System Applications...")
-    utils.open_system_apps()
-
-    typer.echo(f"Opening Installed Applications...")
-    utils.open_installed_apps()
+            typer.echo(f"Opening Installed Applications...")
+            utils.open_apps("system")
+        case "music":
+            typer.echo(f"Opening Music Applications...")
+            utils.open_apps("music")
 
 
 if __name__ == "__main__":
--- my_utils/utils.py
+++ my_utils/utils.py
@@ -12,22 +12,20 @@ def get_json_config(file_name: str) -> dict:
 def open_app(dir: str, app: str) -> int:
     """open a single application"""
     print(f"Dir: {dir} - Opening {app}...")
-    # return system(f"open {dir}/{app}.app")
+    return system(f"open {dir}/{app}.app")
 
 
-def open_installed_apps() -> None:
-    """open default installed applications"""
-    app_list = get_json_config("my-apps")
-    dir = app_list["installed"]["path"]
+def open_apps(type: str) -> None:
+    """open applications
+    
+    Args:
+        type (str): Type of applications to open. Can be "installed" or "system"
+    """
+    if type not in ["installed", "system", "music"]:
+        raise ValueError("Type must be either 'installed' or 'system'")
+    
+    data = get_json_config("my-apps")[type]
+    
+    for app in data["apps"]:
+        open_app(data["path"], app)
 
-    for app in app_list["installed"]["apps"]:
-        open_app(dir, app)
-
-
-def open_system_apps() -> None:
-    """open default system applications"""
-    app_list = get_json_config("my-apps")
-    dir = app_list["system"]["path"]
-
-    for app in app_list["system"]["apps"]:
-        open_app(dir, app)
